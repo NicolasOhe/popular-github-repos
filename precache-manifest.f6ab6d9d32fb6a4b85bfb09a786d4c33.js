c85cdd2eff33d45adc753f6b0b9d7414c1e049c8
Nicolas Cauet <nicolascauetwork@gmail.com>
2020-06-08 10:59:11 +0200

    Updates

---
+++ precache-manifest.f6ab6d9d32fb6a4b85bfb09a786d4c33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cbeea2b67a1ba896f3c9920523562b",
+    "url": "/popular-github-repos/index.html"
+  },
+  {
+    "revision": "5a999397c4de7c9006a8",
+    "url": "/popular-github-repos/static/js/2.2a422762.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/popular-github-repos/static/js/2.2a422762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6b3df229d1bbbb5cfd",
+    "url": "/popular-github-repos/static/js/main.a4f828bd.chunk.js"
+  },
+  {
+    "revision": "cd2c9a283e4544ff94e5",
+    "url": "/popular-github-repos/static/js/runtime-main.6eb48e6c.js"
+  }
+]);
